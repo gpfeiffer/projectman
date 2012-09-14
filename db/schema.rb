37b359ae6847f1e59d610480e7cfa42d8548315d
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-09-14 08:52:59 +0100

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120912194934) do
+ActiveRecord::Schema.define(:version => 20120914075041) do
 
   create_table "lecturers", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(:version => 20120912194934) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "nuig_mail"
+  end
+
+  create_table "students", :force => true do |t|
+    t.string   "first"
+    t.string   "last"
+    t.string   "registration"
+    t.string   "programme"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
