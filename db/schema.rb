f97c6de84cbf85586c7f5026f8c771a7b7289800
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-09-14 17:07:58 +0100

    rails generate scaffold Project title:string student_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120914075041) do
+ActiveRecord::Schema.define(:version => 20120914143948) do
 
   create_table "lecturers", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(:version => 20120914075041) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "nuig_mail"
+  end
+
+  create_table "projects", :force => true do |t|
+    t.string   "title"
+    t.integer  "student_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "students", :force => true do |t|
