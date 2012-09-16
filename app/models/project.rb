b39e418ba5fa4f32f84e1bb197f1d494be0c3feb
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-09-16 20:10:11 +0100

    added supervisor_id method to project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,11 @@
 class Project < ActiveRecord::Base
   belongs_to :student
   has_one :supervision, :dependent => :destroy
-  has_one :supervisor, :through => :supervision, :source => :lecturer 
+  has_one :supervisor, :through => :supervision, :source => :lecturer
 
   validates :title, :student_id, :presence => true
+
+  def supervisor_id
+    supervisor.id if supervisor
+  end
 end
